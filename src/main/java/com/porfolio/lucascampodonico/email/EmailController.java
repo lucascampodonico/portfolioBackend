e614ffd5466bd3abea4b54eca85d4b1d7d03ef17
steerven <lucasjoelcampodonico@gmail.com>
2023-04-26 11:13:42 -0300

    retorno de json en email

--- src/main/java/com/porfolio/lucascampodonico/email/EmailController.java
+++ src/main/java/com/porfolio/lucascampodonico/email/EmailController.java
@@ -1,5 +1,8 @@
 package com.porfolio.lucascampodonico.email;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +23,7 @@ public class EmailController {
     private JavaMailSender javaMailSender;
 
     @PostMapping("/send-email")
-    public String sendEmail(@RequestBody EmailRequest emailRequest) throws AddressException, MessagingException {
+    public Map<String, String> sendEmail(@RequestBody EmailRequest emailRequest) throws AddressException, MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
 
         String html = "<table border='1' style='border-collapse: collapse; width: 100%; height: 54px;'><tbody><tr style='height: 18px;'><td style='width: 100%; height: 18px;'>Nombre:&nbsp;"+emailRequest.getNombre()+"</td></tr><tr style='height: 18px;'><td style='width: 100%; background-color: #f2f2f2; height: 18px;'>Correo:&nbsp;"+emailRequest.getEmail()+"</td></tr><tr style='height: 18px;'><td style='width: 100%; height: 18px;'>Mensaje:&nbsp;"+emailRequest.getMensaje()+"</td></tr></tbody></table>";
@@ -32,8 +35,10 @@ public class EmailController {
         message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse("lucasjoelcampodonico@gmail.com"));
         
         javaMailSender.send(message);
-        
-        return "Email sent successfully!";
+       // Crear y retornar la respuesta JSON como un mapa
+       Map<String, String> response = new HashMap<>();
+       response.put("msg", "Email sent successfully!");
+       return response;
         
     }
 }
